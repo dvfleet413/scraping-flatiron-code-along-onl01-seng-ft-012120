011c46f7a0a54435fb90394f4b4d2bbe5d626205
David Van Fleet <dvfleet413@gmail.com>
2020-02-02 19:09:58 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,36 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
+  
+  def get_page
+    html = open("http://learn-co-curriculum.github.io/site-for-scraping/courses")
+    Nokogiri::HTML(html)
+  end
+  
+  def get_courses
+    doc = get_page
+    doc.css(".post")
+  end
+  
+  def make_courses
+    courses = get_courses
+    courses.each do |course|
+      new_course = Course.new
+      course.children.each do |element|
+        if element.name == 'h2'
+          new_course.title = element.text
+        elsif element.name == "em"
+          new_course.schedule = element.text
+        elsif element.name == "p"
+          new_course.description = element.text
+        end
+      end
+    end
+  end
   
   def print_courses
     self.make_courses
